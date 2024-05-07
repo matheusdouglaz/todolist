bf8dabb3100020fc95732253f45fe2447d2f82c0
matheusdouglaz <matheustecnologi@outlook.com>
2024-05-07 12:41:31 -0300

    Alteração do background, estilização, acrescentei butões para facilitar a fitragem das demandas

--- scripts.js
+++ scripts.js
@@ -1,65 +1,141 @@
-const button = document.querySelector('.button-add-task')
-const input = document.querySelector('.input-task')
-const listaCompleta = document.querySelector('.list-tasks')
-
-let minhaListaDeItens = []
+const button = document.querySelector('.button-add-task');
+const input = document.querySelector('.input-task');
+const listaCompleta = document.querySelector('.list-tasks');
+const filterButtonsContainer = document.querySelector('.filter-buttons');
+const filterButtonToggle = document.querySelector('.button-toggle-filter');
+const filterButtons = document.querySelectorAll('.button-filter');
+let minhaListaDeItens = [];
 
 function adicionarNovaTarefa() {
   minhaListaDeItens.push({
     tarefa: input.value,
     concluida: false,
-  })
+    editando: false
+  });
 
-  input.value = ''
+  input.value = '';
 
-  mostrarTarefas()
+  mostrarTarefas(minhaListaDeItens);
 }
 
-function mostrarTarefas() {
-  let novaLi = ''
+function mostrarTarefas(lista) {
+  let novaLi = '';
 
-  // ['comprar café', 'estudar programação']
+  lista.forEach((item, posicao) => {
+    const concluidaClass = item.concluida ? 'done' : '';
+    const editandoClass = item.editando ? 'editando' : '';
 
-  minhaListaDeItens.forEach((item, posicao) => {
-    novaLi =
-      novaLi +
-      `
-
-        <li class="task ${item.concluida && 'done'}">
+    if (item.editando) {
+      // Se o item está em modo de edição, mostra o campo de edição
+      novaLi += `
+        <li class="task ${concluidaClass} ${editandoClass}">
+          <div class="botoes">
             <img src="./img/checked.png" alt="check-na-tarefa" onclick="concluirTarefa(${posicao})">
-            <p>${item.tarefa}</p>
             <img src="./img/trash.png" alt="tarefa-para-o-lixo" onclick="deletarItem(${posicao})">
+            <img src="./img/editar.png" alt="editar-tarefa" onclick="editarTarefa(${posicao})">
+          </div>
+          <input type="text" class="editar-tarefa" value="${item.tarefa}" onkeydown="salvarEdicaoTecla(event, ${posicao})">
         </li>
-        
-        `
-  })
+      `;
+    } else {
+      // Caso contrário, mostra apenas o texto da tarefa
+      novaLi += `
+        <li class="task ${concluidaClass} ${editandoClass}">
+        <p>${item.tarefa}</p>
+          <div class="botoes">
+            <img src="./img/checked.png" alt="check-na-tarefa" onclick="concluirTarefa(${posicao})">
+            <img src="./img/trash.png" alt="tarefa-para-o-lixo" onclick="deletarItem(${posicao})">
+            <img src="./img/editar.png" alt="editar-tarefa" onclick="editarTarefa(${posicao})">
+          </div>
+          
+        </li>
+      `;
+    }
+  });
 
-  listaCompleta.innerHTML = novaLi
-
-  localStorage.setItem('lista', JSON.stringify(minhaListaDeItens))
+  listaCompleta.innerHTML = novaLi;
 }
 
 function concluirTarefa(posicao) {
-  minhaListaDeItens[posicao].concluida = !minhaListaDeItens[posicao].concluida
-
-  mostrarTarefas()
+  minhaListaDeItens[posicao].concluida = !minhaListaDeItens[posicao].concluida;
+  mostrarTarefas(minhaListaDeItens);
 }
 
 function deletarItem(posicao) {
-  minhaListaDeItens.splice(posicao, 1)
-
-  mostrarTarefas()
+  minhaListaDeItens.splice(posicao, 1);
+  mostrarTarefas(minhaListaDeItens);
 }
 
 function recarregarTarefas() {
-  const tarefasDoLocalStorage = localStorage.getItem('lista')
-
+  const tarefasDoLocalStorage = localStorage.getItem('lista');
   if (tarefasDoLocalStorage) {
-    minhaListaDeItens = JSON.parse(tarefasDoLocalStorage)
+    minhaListaDeItens = JSON.parse(tarefasDoLocalStorage);
   }
-
-  mostrarTarefas()
+  mostrarTarefas(minhaListaDeItens);
 }
 
-recarregarTarefas()
-button.addEventListener('click', adicionarNovaTarefa)
+function editarTarefa(posicao) {
+  // Primeiro, desativa o modo de edição para todos os itens
+  minhaListaDeItens.forEach(item => {
+    item.editando = false;
+  });
+
+  // Em seguida, ativa o modo de edição apenas para o item clicado
+  minhaListaDeItens[posicao].editando = true;
+
+  mostrarTarefas(minhaListaDeItens);
+}
+
+function salvarEdicao(posicao) {
+  const novoTexto = document.querySelector(`.list-tasks li:nth-child(${posicao + 1}) input.editar-tarefa`).value;
+  minhaListaDeItens[posicao].tarefa = novoTexto;
+  minhaListaDeItens[posicao].editando = false;
+  mostrarTarefas(minhaListaDeItens);
+}
+
+function salvarEdicaoTecla(event, posicao) {
+  if (event.key === 'Enter') {
+    const novoTexto = event.target.value;
+    minhaListaDeItens[posicao].tarefa = novoTexto;
+    minhaListaDeItens[posicao].editando = false;
+    mostrarTarefas(minhaListaDeItens);
+  }
+}
+
+function filtrarTarefas(filtro) {
+  let listaFiltrada = [];
+
+  switch (filtro) {
+    case 'todas':
+      listaFiltrada = minhaListaDeItens;
+      break;
+    case 'a-fazer':
+      listaFiltrada = minhaListaDeItens.filter(item => !item.concluida);
+      break;
+    case 'concluidas':
+      listaFiltrada = minhaListaDeItens.filter(item => item.concluida);
+      break;
+  }
+
+  mostrarTarefas(listaFiltrada);
+}
+
+recarregarTarefas();
+button.addEventListener('click', adicionarNovaTarefa);
+input.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    adicionarNovaTarefa();
+  }
+});
+
+filterButtonToggle.addEventListener('click', () => {
+  const currentDisplay = filterButtonsContainer.style.display;
+  filterButtonsContainer.style.display = currentDisplay === 'none' ? 'block' : 'none';
+});
+
+filterButtons.forEach(button => {
+  button.addEventListener('click', function() {
+    const filtro = this.dataset.filtro;
+    filtrarTarefas(filtro);
+  });
+});
